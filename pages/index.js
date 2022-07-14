d45d81d8f8c0b1a3a895c57c4a33cae60c45f5ee
Yusuf Apriliyanto <97005861+Yusufapriliyanto@users.noreply.github.com>
2022-07-14 09:05:28 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,20 +12,50 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"/>
       </Head>
-
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <navbar className="navbar navbar-expand-lg bg-white">
-          <div className="nav-link">
-            <Linker href="/"> Halaman Depan </Linker>
+{/* Header */}
+      <h1 className={styles.title}>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+{/* Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider"></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
           </div>
-          <div className="nav-link">
-            <Linker href="/h2"> Halaman Kedua </Linker>
-          </div>
-        </navbar>
-        <p> Ini adalah contoh situs yang dibuat menggunakan NextJS</p>
-
+        </div>
+      </nav>
+{/* Konten Index*/}
+      <p> Ini adalah contoh situs yang dibuat menggunakan NextJS</p>
+{/* Footer */}
       <div className="footer mt-auto py-3 bg-light">
         <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer" className="container">
           Powered by{' '}
