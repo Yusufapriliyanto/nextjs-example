ab2e7be63aa828c509e7a819513b02fcc7483b24
Yusuf Apriliyanto <97005861+Yusufapriliyanto@users.noreply.github.com>
2022-07-14 10:26:02 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
       <div className="footer mt-auto py-3 bg-light">
         <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer" className="container">
           Powered by{' '}
-          <span className={styles.logo}>
+          <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
