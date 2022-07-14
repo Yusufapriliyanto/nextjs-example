23919afaaf4d111782877a377f82b6c1895a07b9
Yusuf Apriliyanto <97005861+Yusufapriliyanto@users.noreply.github.com>
2022-07-14 10:24:52 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"/>
       </Head>
 {/* Header */}
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <h1> Welcome to
+        <a href="https://nextjs.org">Next.js!</a>
       </h1>
 {/* Navbar */}
       <nav className="navbar navbar-expand-lg navbar-light bg-light mx-auto">
