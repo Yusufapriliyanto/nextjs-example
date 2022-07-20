e359f924403655f423eefa2ddc1696411d5761f3
Yusuf Apriliyanto <97005861+Yusufapriliyanto@users.noreply.github.com>
2022-07-20 16:07:19 +0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,12 +4,9 @@ export default function handler(req, res) {
   res.status(200).json(
     {
       'sambutan1': 'Hai, mau tau data yang mendukung pembuatan situs ini? Tidak semuanya terbuka ya.',
-      'sambutan2': '/api berisi data-data yang boleh dibaca dan digunakan oleh siapapun.',
-      'sambutan3': 'Jangan cari data yang lain ya. Data lain hanya dipakai untuk keperluan internal.',
-      'sambutan4': 'Beberapa data dienkripsi dan ditampilkan sebagai string acak. Silahkan cari sendiri cara membukanya di internet.',
-      'data':{
-        'name': 'John Doe'
-      }
+      'sambutan2': '/api berisi beberapa data terbuka dan beberapa data internal yang terenkripsi',
+      'sambutan3': 'Beberapa data dienkripsi dan ditampilkan sebagai string acak. Jika Anda penasaran, silahkan cari sendiri cara membukanya di internet.',
+      'sambutan4': '/api/hello hanya berisi sambutan ini. Cari harta karunnya di path lain'
     }
   )
 }
