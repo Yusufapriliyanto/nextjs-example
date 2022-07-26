ef82bcb78184eed850f2cdc3946e69ab5c0fb1c5
Yusufapriliyanto <yusufapriliyanto74@gmail.com>
2022-07-26 09:13:56 +0700

    git-commit#4

--- pages/diary_pribadi.js
+++ pages/diary_pribadi.js
@@ -35,32 +35,7 @@ export default function Home() {
             </ul>
         </div>
       </nav>
-{/* Konten Index*/}
-      <p> Ini adalah contoh situs yang dibuat menggunakan NextJS</p>
-      <div className={styles.grid_container}>
-        <div className={styles.item1}> Katalog Konten Pilihan </div>
-      </div>
-{/* Daftar Konten Fiksi*/}
-      <p> Daftar Diary Pribadi dan Konten Fiksi </p>
-      <div className={styles.grid_container}>
-        <div className={styles.item1}> <Linker href="/diary_pribadi/p1"> Diary-p1 </Linker> </div>
-        <div className={styles.item1}> <Linker href="/diary_pribadi/p2"> Diary-p2 </Linker> </div>
-        <div className={styles.item1}> <Linker href="/diary_pribadi/p3"> Diary-p3 </Linker> </div>
-      </div>
-      <div className={styles.grid_container}>
-        <div className={styles.item1}> <Linker href="/konten_fiksi/p1"> Konten-p1 </Linker> </div>
-        <div className={styles.item1}> <Linker href="/konten_fiksi/p2"> Konten-p2 </Linker> </div>
-        <div className={styles.item1}> <Linker href="/konten_fiksi/p3"> Konten-p3 </Linker> </div>
-      </div>
-{/* Footer */}
-      <div className="footer mt-auto py-3 bg-blue">
-        <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer" className="nav-item nav-link">
-          Powered by{' '}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </div>
+{/* Konten Halaman */}
     </div>
   )
 }
